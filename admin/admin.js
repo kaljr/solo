681710e57da2e2cc3c8fdd99fe1e6fb57f9c619a
Kenny Langston <kenny.langston@gmail.com>
2015-04-27 14:27:33 -0700

    Working communication between client and server

--- admin/admin.js
+++ admin/admin.js
@@ -10,9 +10,11 @@ var io = socketio(server);
 // listen on port 3000
 server.listen(3000);
 
+console.log('listening on port 3000');
 // when there is a connection, do this
 io.on('connection', function (socket) {
-  io.emit('connectMessage', {
+  console.log('someone connected');
+  socket.emit('connectMessage', {
     message: 'you are connected'
   });
 });
--- q.js
+++ q.js
@@ -1,8 +1,8 @@
 // open socket to server
-var socket = io.connect('http://kenemon.com/quizzr/quiz');
+var socket = io.connect('http://kenemon.com:3000');
 
 // on connection
-socket.on('connect', function (data) {
+socket.on('connectMessage', function (data) {
     console.log(data);
     //socket.emit('my other event', { my: 'clientHTML' });
   });
